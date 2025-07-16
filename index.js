c682f622928f2f9ef137f811293e3dcbc6af983c
jsedubarros@gmail.com <jsedubarros@gmail.com>
2025-07-16 10:48:39 -0700

    update index

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const express = require('express');
-const {sequelize} = require('./src/config/configDB');
-const ProdutoModel = require('./src/modules/produto/models/produtoModel');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const cors = require('cors')
+const { sequelize } = require('./src/config/configDB');
+const authRoute = require('./src/modulos/autenticacao/routes/autenticacao.route')
+const usuarioRoute = require('./src/modulos/usuario/routes/usuario.route')
+// Configuração do banco de dados
+dotenv.config(); // Carrega variáveis de ambiente do arquivo .env
+
 const app = express();
-const port = process.env.PORT;
+app.use(cors({
+    origin: 'http://localhost:5173', // frontend React
+    credentials: true               // permite enviar cookies (como refreshToken)
+}));
+
 app.use(express.json());
 
-sequelize.sync()
-  .then(async () => {
+// rotas de usuario
+// http:localhost:3001/api/cadastrar
+// http:localhost:3001/api/perfil
+app.use('/api/', usuarioRoute)
+
+// rotas de autenticação
+// http:localhost:3001/api/login
+// http:localhost:3001/api/logout
+// http:localhost:3001/api/refresh-token
+app.use('/api/', authRoute)
+
+const PORTA = process.env.PORTA;
+app.listen(PORTA, async () => {
     try {
-      await sequelize.authenticate();
-      console.log('Conexão com o banco de dados estabelecida com sucesso.');
-      
-      app.listen(port, () => {
-        console.log(`Servidor rodando na porta ${port}`);
-      });
-    } catch (error) {
-      console.error('Não foi possível conectar ao banco de dados:', error);
+        await sequelize.authenticate();
+        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+
+        await sequelize.sync({ force: true, alter: true });
+        console.log('Banco de dados sincronizado com sucesso.');
+    } catch (err) {
+        console.error('Erro ao conectar ou sincronizar o banco de dados:', err);
     }
-  })
-  .catch((syncError) => {
-    console.error('Erro ao sincronizar com o banco de dados:', syncError);
-  });
+    console.log(`Servidor rodando na porta ${PORTA}`);
+});
